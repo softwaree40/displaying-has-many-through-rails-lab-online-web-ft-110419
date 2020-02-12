9a5a9b91a4b79e55fc3daf9b6917e7c347161a9c
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-12 05:29:55 +0000

    Done.

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,8 @@
 class Patient < ApplicationRecord
       has_many :appointments
       has_many :doctors, through: :appointments
+      
+  def get_number_of_appointments
+    self.appointments.length
+  end
 end
