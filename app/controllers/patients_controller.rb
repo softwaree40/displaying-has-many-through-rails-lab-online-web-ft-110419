b38c1a6989e4792c95ffa93e9b98420cba8c8d06
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-12 05:39:32 +0000

    Automatically backed up by Learn

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -8,20 +8,4 @@ class PatientsController < ApplicationController
     @patient = Patient.find(params[:id])
   end
   
-  def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
-
-  
 end
